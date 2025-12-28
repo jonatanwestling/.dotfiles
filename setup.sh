88c5a9f97edfa77c41e8aefc9a16274a57bd057b
Jonatan Westling <jonatanwestling4@gmail.com>
2025-12-28 15:31:38 +0100

    fix: update to setup scripts folder

--- setup.sh
+++ setup.sh
@@ -8,35 +8,35 @@ LINK_COUNT=0    # symlinks created/overwritten
 SKIPPED_COUNT=0 # symlinks skipped
 
 create_symlink() {
-	local src="$1"
-	local dest="$2"
+  local src="$1"
+  local dest="$2"
 
-	if [ -L "$dest" ] && [ "$(readlink "$dest")" = "$src" ]; then
-		echo "➜ $dest already correctly symlinked, skipping."
-		((LINK_COUNT))
-	elif [ -e "$dest" ]; then
-		echo
-		echo "❗$dest exists but is not the expected symlink."
-		read -p "❗Do you want to overwrite it? [y/N] " answer
-		case "$answer" in
-		[Yy]*)
-			rm -rf "$dest"
-			ln -s "$src" "$dest"
-			echo
-			echo "✔ Overwritten: $dest → $src"
-			((LINK_COUNT))
-			;;
-		*)
-			echo
-			echo "➜ Skipped: $dest"
-			((SKIPPED_COUNT))
-			;;
-		esac
-	else
-		ln -s "$src" "$dest"
-		echo "✔ Created symlink: $dest → $src"
-		((LINK_COUNT))
-	fi
+  if [ -L "$dest" ] && [ "$(readlink "$dest")" = "$src" ]; then
+    echo "➜ $dest already correctly symlinked, skipping."
+    ((LINK_COUNT))
+  elif [ -e "$dest" ]; then
+    echo
+    echo "❗$dest exists but is not the expected symlink."
+    read -p "❗Do you want to overwrite it? [y/N] " answer
+    case "$answer" in
+    [Yy]*)
+      rm -rf "$dest"
+      ln -s "$src" "$dest"
+      echo
+      echo "✔ Overwritten: $dest → $src"
+      ((LINK_COUNT))
+      ;;
+    *)
+      echo
+      echo "➜ Skipped: $dest"
+      ((SKIPPED_COUNT))
+      ;;
+    esac
+  else
+    ln -s "$src" "$dest"
+    echo "✔ Created symlink: $dest → $src"
+    ((LINK_COUNT))
+  fi
 }
 
 # Zsh config
@@ -62,6 +62,10 @@ mkdir -p "$HOME/.config/ghostty"
 
 create_symlink "$DOTFILES_DIR/ghostty.config" "$HOME/.config/ghostty/config"
 create_symlink "$DOTFILES_DIR/Ghostty.icns" "$HOME/.config/ghostty/Ghostty.icns"
+
+#Personal scripts folder
+mkdir -p "$HOME/.local" # make sure parent exists
+create_symlink "$DOTFILES_DIR/scripts" "$HOME/.local/scripts"
 
 echo
 echo "🔗 Summary:"
